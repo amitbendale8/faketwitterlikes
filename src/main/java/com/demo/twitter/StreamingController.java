8c71095ce0444a546d5f3a5718c0f037010f9568
Amit Bendale <bendale.amit7@gmail.com>
2017-03-03 21:47:08 +0530

    Added Streaming

--- src/main/java/com/demo/twitter/StreamingController.java
+++ src/main/java/com/demo/twitter/StreamingController.java
@@ -1,13 +1,27 @@
 package com.demo.twitter;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
-import ch.qos.logback.core.status.StatusListener;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.entity.TwitterUser;
+import com.demo.utility.TwitterUtility;
+
+import twitter4j.FilterQuery;
+import twitter4j.StallWarning;
 import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
+import twitter4j.User;
+import twitter4j.conf.ConfigurationBuilder;
 
+@RestController
 public class StreamingController {
 	 /**
      * Display Users
@@ -16,24 +30,73 @@ public class StreamingController {
      */
     @RequestMapping("/listen")
     public String listen() {
-    		StatusListener listener = new StatusListener(){
+    	System.out.println("Started Listening");
+    	TwitterUser user = TwitterUtility.getUser();
+    	ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+        cb.setOAuthConsumerKey(user.getConsumerKey());
+        cb.setOAuthConsumerSecret(user.getConsumerSecret());
+        cb.setOAuthAccessToken(user.getAccessToken());
+        cb.setOAuthAccessTokenSecret(user.getAccessTokenSecret());
+
+        TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+
+        StatusListener listener = new StatusListener() {
+
+            @Override
+            public void onException(Exception arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onScrubGeo(long arg0, long arg1) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
             public void onStatus(Status status) {
-                System.out.println(status.getUser().getName() + " : " + status.getText());
+                User user = status.getUser();
+                
+                // gets Username
+                String username = status.getUser().getScreenName();
+                System.out.println(username);
+                String profileLocation = user.getLocation();
+                System.out.println(profileLocation);
+                long tweetId = status.getId(); 
+                System.out.println(tweetId);
+                String content = status.getText();
+                System.out.println(content +"\n");
+
             }
-            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-            public void onException(Exception ex) {
-                ex.printStackTrace();
+
+            @Override
+            public void onTrackLimitationNotice(int arg0) {
+                // TODO Auto-generated method stub
+
             }
+
 			@Override
-			public void addStatusEvent(ch.qos.logback.core.status.Status arg0) {
+			public void onStallWarning(StallWarning arg0) {
 				// TODO Auto-generated method stub
 				
 			}
+
         };
-        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        FilterQuery fq = new FilterQuery();
+    
+        
+        //fq.follow(new long[] { 12354654});
+        fq.follow(new long[] {1021626787});
         twitterStream.addListener(listener);
-        // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
-        twitterStream.sample();
+        twitterStream.filter(fq);  
+        return null;
     }
 }
